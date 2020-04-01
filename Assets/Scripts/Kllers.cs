0435eb606ee20df50648b63d808eceab80d24232
T3MPGAMER <62100647+T3MPGAMER@users.noreply.github.com>
2020-04-01 01:12:04 +0100

    Add files via upload

---
+++ Assets/Scripts/Kllers.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Kllers : MonoBehaviour
+{
+    public GameObject Player;
+    public float movementSpeed = 3;
+    void Update()
+    {
+        transform.LookAt(Player.transform);
+        transform.position += transform.forward * movementSpeed * Time.deltaTime;
+    }
+}
+
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject bulletClass;
+    Vector3 spawnPosition;
+    Quaternion spawnRotation;
+    public int bulletCount = 20;
+    const float rateOfFire = 0.2f;
+    const float speed = 2000f;
+    float fireDelay;
+    void FixedUpdate()
+    {
+        if (Input.GetButton("Fire1") && Time.time > fireDelay && bulletCount > 0)
+        {
+            spawnPosition = transform.position; spawnRotation = transform.rotation; //* Quaternion.Euler(90,0,0); 
+            fireDelay = Time.time + rateOfFire;
+            GameObject bulletInstance = Instantiate(bulletClass, spawnPosition, spawnRotation);// transform.up or transform.forward depending on angle of bullet     
+            bulletInstance.GetComponent<Rigidbody>().AddForce(transform.forward * speed);      //Physics.IgnoreCollision(bulletInstance.GetComponent<Collider>(), GetComponent<Collider>());     
+            bulletCount--;
+        }
+    }
+}
